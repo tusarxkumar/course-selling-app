393aa1e966bd04888e35ad019ae9d7ee9a174ebf
Tusar kumar Parida <tusarkumarparida9@gmail.com>
2024-12-01 21:52:08 +0530

    index.js updated

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const {courseRouter} = require("./routes/course")
 
 app.use('/api/v1/user',userRouter)
 app.use('/api/v1/course',courseRouter)
+app.use('/api/v1/course',courseRouter1)
+app.use('/api/v1/course',courseRouter2)
 
 app.listen(3000);
 
